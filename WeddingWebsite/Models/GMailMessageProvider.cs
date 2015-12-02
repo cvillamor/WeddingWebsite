7218992161ba049796badfb8e34e598b25ecdda1
cvillamor <christian.villamor@gmail.com>
2015-12-02 08:33:15 -0500

    Changed EmailMessageProvider to GMailMessageProvider

--- WeddingWebsite/Models/GMailMessageProvider.cs
+++ WeddingWebsite/Models/GMailMessageProvider.cs
@@ -5,8 +5,12 @@ using System.Web;
 
 namespace WeddingWebsite.Models
 {
-  public class EmailMessageProvider : IMessageProvider
+  public class GmailMessageProvider : IMessageProvider
   {
+    public GmailMessageProvider()
+    {
+    }
+
     public bool SendMessage(IMessage message)
     {
       throw new NotImplementedException();
