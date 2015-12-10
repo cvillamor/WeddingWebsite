abc7c5ba0dea43113418f523117911a0b5d87733
cvillamor <christian.villamor@gmail.com>
2015-12-09 22:12:35 -0500

    Test emails

---
+++ WeddingWebsite.Tests/EmailTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Net;
+using System.Net.Mail;
+
+namespace WeddingWebsite.Tests
+{
+  public class EmailTest
+  {
+    [Fact]
+    public void SendEmail()
+    {
+      var fromAddress = new MailAddress("@gmail.com", "");
+      var toAddress = new MailAddress("", "");
+      var toAddress2 = new MailAddress("", "");
+
+      List<MailAddress> mailAddresses = new List<MailAddress>() { toAddress, toAddress2 };
+      const string fromPassword = "";
+      const string subject = "Test Email";
+      const string body = "Derp";
+
+      var smtp = new SmtpClient
+      {
+        Host = "smtp.gmail.com",
+        Port = 587,
+        EnableSsl = true,
+        DeliveryMethod = SmtpDeliveryMethod.Network,
+        Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+        Timeout = 20000
+      };
+
+      foreach(var mailingAddress in mailAddresses)
+      { 
+        using (var message = new MailMessage(fromAddress, mailingAddress)
+        {
+          Subject = subject,
+          Body = body
+        })
+        {
+          try
+          { 
+            smtp.Send(message);
+          }
+          catch(Exception ex)
+          {
+
+          }
+        }
+      }
+    }
+
+  }
+}
