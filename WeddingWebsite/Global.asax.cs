1326d261dc4ab27ed380b96aeabd167e22eb26e6
cvillamor <christian.villamor@gmail.com>
2015-12-12 12:37:12 -0500

    Added Ninject.Web.Common for Kernel creation on Website start

--- WeddingWebsite/Global.asax.cs
+++ WeddingWebsite/Global.asax.cs
@@ -3,18 +3,35 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Ninject.Web.Common;
+using Ninject;
+using System.Reflection;
+using WeddingWebsite.Models.Ninject_Modules;
 
 namespace WeddingWebsite
 {
-    public class MvcApplication : HttpApplication
+  public class MvcApplication : NinjectHttpApplication
+  {
+
+    protected void Application_Start()
     {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
+      AreaRegistration.RegisterAllAreas();
+      GlobalConfiguration.Configure(WebApiConfig.Register);
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
     }
+
+    /// <summary>
+    /// Create Kernel call
+    /// </summary>
+    /// <returns>An instanced version of the StandardKernel for Ninject</returns>
+    protected override IKernel CreateKernel()
+    {
+      var kernel = new StandardKernel(new GmailModule());
+      kernel.Load(Assembly.GetExecutingAssembly());
+
+      return kernel;
+    }
+  }
 }
---
+++ WeddingWebsite/Models/Ninject Modules/GmailEmailClientBinding.cs
@@ -0,0 +1,37 @@
+﻿using Ninject.Modules;
+using WeddingWebsite.Models.Providers;
+using WeddingWebsite.Models.Providers.Email;
+using System.Configuration;
+using WeddingWebsite.Models.DataObjects;
+using System.Net.Mail;
+using System.Linq;
+
+namespace WeddingWebsite.Models.Ninject_Modules
+{
+  /// <summary>
+  /// GMail EmailClient Binding
+  /// </summary>
+  public class GmailModule : NinjectModule
+  {
+    public override void Load()
+    {
+      //IEmailClientSender
+       Bind<IEmailClientSender>()
+        .To<GmailClientSender>()
+        .WhenInjectedInto<GmailMessageProvider>()
+        .WithConstructorArgument("fromAddress", ConfigurationManager.AppSettings["MessageFrom"])
+        .WithConstructorArgument("password", ConfigurationManager.AppSettings["EmailPassword"]);
+
+      //IEmailClientRecepients
+      //Addresses
+      var addresses = ConfigurationManager.AppSettings["MessageTo"]
+                        .Split(',')
+                        .Select(m => new MailAddress(m));
+
+      Bind<IEmailClientRecipients>()
+        .To<GmailClientReceiver>()
+        .WhenInjectedInto<GmailMessageProvider>()
+        .WithConstructorArgument("addresses", addresses);      
+    }
+  }
+}
