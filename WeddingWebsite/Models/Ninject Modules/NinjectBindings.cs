86475bdbc7e112787ce293b1e5d1c19ddaf7861b
cvillamor <christian.villamor@gmail.com>
2015-12-11 20:52:45 -0500

    Ninject binding class added

---
+++ WeddingWebsite/Models/Ninject Modules/NinjectBindings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Modules;
+using WeddingWebsite.Models.Providers;
+using WeddingWebsite.Models.Providers.Email;
+
+namespace WeddingWebsite.Models.Ninject_Modules
+{
+  public class NinjectBindings : NinjectModule
+  {
+    public override void Load()
+    {
+      Bind<IEmailClientBuilder>().To<GmailClientBuilder>();
+    }
+  }
+}
