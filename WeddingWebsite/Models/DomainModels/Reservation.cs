3230850723177ef321f858152ac20194b0daeb81
cvillamor <christian.villamor@gmail.com>
2015-12-13 08:28:31 -0500

    Reservation DomainModel initial check in

---
+++ WeddingWebsite/Models/DomainModels/Reservation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeddingWebsite.Models.DomainModels
+{
+  public class Reservation
+  {
+    public string Number { get; set; }
+  }
+}
