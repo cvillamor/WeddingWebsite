a2e0b2c6d6b46ca6014875d239e4c9f509ef7b7c
cvillamor <christian.villamor@gmail.com>
2015-12-13 08:15:49 -0500

    Person Repository checkin

---
+++ WeddingWebsite/Models/Repository/PersonRepository.cs
@@ -0,0 +1,46 @@
+﻿using Common.Logging;
+using LinqToDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeddingWebsite.Models.DTO;
+
+namespace WeddingWebsite.Models.Repository
+{
+  public class PersonRepository : Repository<Person>, IPersonRepository
+  {
+    private ILog _log = LogManager.GetLogger<PersonRepository>();
+
+    public PersonRepository(IDataContext dataContext)
+      : base(dataContext)
+    { }
+
+    /// <summary>
+    /// Check if the Phone number exists in the database
+    /// </summary>
+    /// <param name="phoneNumber"></param>
+    /// <returns></returns>
+    public bool CheckPhoneNumberExists(string phoneNumber)
+    {
+      bool success = false;
+
+      try
+      {
+        success = this.Find(m => m.PhoneNumber == phoneNumber).Count() > 0;
+
+        if (success)
+          _log.InfoFormat("Number {0} found in database", phoneNumber);
+        else
+          _log.InfoFormat("Number {0} NOT found in database", phoneNumber);
+      }
+      catch (Exception ex)
+      {
+        _log.ErrorFormat("There was an error trying to access PersonTable {0}", ex.Message);
+        success = false;
+      }
+
+      return success;
+    }
+  }
+}
