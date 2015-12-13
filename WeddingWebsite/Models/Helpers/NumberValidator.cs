56cc5f3b40d36a17f905b489ec0f82fa43daa6b5
cvillamor <christian.villamor@gmail.com>
2015-12-13 08:39:50 -0500

    NumberValidator helper

---
+++ WeddingWebsite/Models/Helpers/NumberValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace WeddingWebsite.Models.Helpers
+{
+  public class NumberValidator
+  {
+    public static bool IsPhoneNumber(string number)
+    {
+      return Regex.Match(number, @"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}").Success;
+    }
+  }
+}
