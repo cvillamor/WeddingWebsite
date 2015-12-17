f222b940524cf1fcf43e376fe2583cd0c9c25acc
cvillamor <christian.villamor@gmail.com>
2015-12-16 20:32:36 -0500

    PersonDomain domain model

---
+++ WeddingWebsite/Models/DomainModels/PersonDomain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeddingWebsite.Models.Enums;
+
+namespace WeddingWebsite.Models.DomainModels
+{
+  public class PersonDomain
+  {
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public string Instagram { get; set; }
+    public bool IsInvitedToRehearsalDinner { get; set; }
+    public RSVPStatusEnums WeddingRSVPStatus { get; set; }
+    public RSVPStatusEnums RehearsalDinnerRSVPStatus { get; set; }
+  }
+}
