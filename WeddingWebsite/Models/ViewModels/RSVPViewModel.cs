62275ab3da302c6eedd9ae0cf3106ce17e8260ea
cvillamor <christian.villamor@gmail.com>
2015-12-18 22:52:36 -0500

    RSVPViewModel

---
+++ WeddingWebsite/Models/ViewModels/RSVPViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeddingWebsite.Models.DomainModels;
+
+namespace WeddingWebsite.Models.ViewModels
+{
+  public class RSVPViewModel
+  {
+    public List<PersonDomain> Persons { get; set; }
+    public List<RSVPDomain> RSVPStatuses { get; set; }
+  }
+}
